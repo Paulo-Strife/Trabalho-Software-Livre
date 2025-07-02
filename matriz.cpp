c4b5c7b0841c6688e72288203d3022b8699ecdac
Paulo-Strife <gomespc798@gmail.com>
2025-07-02 01:21:51 -0300

    implementando o for final para representar a matriz e correção de erros

--- matriz.cpp
+++ matriz.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 #include <vector>
+
 using namespace std;
 
-int main(){
+int main() {
     int n;
     cin >> n;
 
@@ -10,43 +11,45 @@ int main(){
     vector<vector<int>> B(n, vector<int>(n));
     vector<vector<int>> resultado(n, vector<int>(n, 0));
 
-    for (int i = 0; i < n; i++){
-        for (int j = 0; j < n; j++){
-            cin >> a[i][j];
-        }
-    }
-    for (int i = 0; i < n; i++){
-        for (int j = 0; j < n; j++){
-            cin >> b[i][j];
-        }
-    }
+    // Lê a primeira matriz
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+            cin >> A[i][j];
+
+    // Lê a segunda matriz
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+            cin >> B[i][j];
+
     char operacao;
     cin >> operacao;
 
-    // essa é a parte da soma.
-    if (operacao == 'a')[
-        for (int i = 0; i < n; i++){
-            for (int j = 0; j < n; j++){
-                resultado[i][j] = a[i][j] + b[i][j];
-            }
-        }
-        // aqui é feita a subtração
-    ]else if (operacao == 's'){
-        for (int i = 0; i < n; i++){
-            for (int j = 0; j < n; j++){
-                resultado[i][j] = a[i][j] + b[i][j];
-            }
-        }
-        //aqui é feita a multiplicação
-    }else if (operacao == 'm'){
-        for (int i = 0; i < n; i++){
-            for (int j = 0; j < n; j++){
-                for (int h = 0; h < n; h++){
-                    resultado[i][j] = a[i][j] + b[h][j];
-                }
-            }
-        }
+    if (operacao == 'a') {
+        // Essa é a parte da soma
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++)
+                resultado[i][j] = A[i][j] + B[i][j];
     }
-    
+    else if (operacao == 's') {
+        // aqui é feita a subtração
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++)
+                resultado[i][j] = A[i][j] - B[i][j];
+    }
+    else if (operacao == 'm') {
+        // aqui é feita a multiplicação
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++)
+                for (int k = 0; k < n; k++)
+                    resultado[i][j] += A[i][k] * B[k][j];
+    }
+
+    // resultado da matriz
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++)
+            cout << resultado[i][j] << " ";
+        cout << endl;
+    }
+
     return 0;
 }
