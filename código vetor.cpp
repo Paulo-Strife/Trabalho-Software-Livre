4a89e51c30b9268deec98819150c1e1a71920b5a
Paulo-Strife <gomespc798@gmail.com>
2025-07-02 01:28:23 -0300

    Implementando código para o vetor

---
+++ código vetor.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+
+    for (int i = 0; i < n; i++){
+        int x;
+        cin >> x;
+        if (x > maior){
+            maior = x;
+        }
+    }
+    
+    
+    
+    return 0;
+}
